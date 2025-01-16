6ecd279ac9c68807160e598ded2eaa70eb948813
yasin-uzun <63108473+yasin-uzun@users.noreply.github.com>
2025-01-16 10:14:29 -0500

    Update run_example_2.sh

--- example_2/run_example_2.sh
+++ example_2/run_example_2.sh
@@ -6,7 +6,7 @@ output_dir=/home/programs/arvin/example_2/output/
 
 /home/programs/arvin/scripts/arvin_wrapper.sh \
     $input_dir/input_snps_file.bed \
-    /home/programs/arvin/arvin_annotation_data/built_in_ep_data/EP_AUTOIMMUNE.bed \
+    $input_dir/EP_AUTOIMMUNE.bed \
     $input_dir/gwava_features_file.tsv \
     $input_dir/funseq_features_file.bed \
     T1D \
